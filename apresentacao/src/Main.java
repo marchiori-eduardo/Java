39a6d91ddaf29acb1ab69b93b81c070834146815
Eduardo Marchiori <145299156+marchiori-eduardo@users.noreply.github.com>
2025-05-25 16:48:16 -0300

    .

---
+++ apresentacao/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String nomeCompleto = "Eduardo Lima Marchiori";
+        int idade = 18;
+
+        System.out.print("Olá Mundo! ");
+        System.out.println("Bom dia!");
+        System.out.println("Meu nome é " + nomeCompleto + " tenho " + idade + " anos.");
+    }
+}
