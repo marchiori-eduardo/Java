3dd89dbb55ace8f38109ef29dbafa02398d5141a
Eduardo Marchiori <145299156+marchiori-eduardo@users.noreply.github.com>
2025-05-25 19:10:38 -0300

    atualizações

--- teste-processamento/src/Main.java
+++ teste-processamento/src/Main.java
@@ -1,15 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+    double b, B, h, area;
+    b = 6.0;
+    B = 8.0;
+    h = 5.0;
+
+    area = (b + B) / 2.0 * h;
+
+    System.out.println(area);
     }
 }
