cd954b265171542c70b704945163fdc2fa70f9bb
marchiori-eduardo <marchiorimarchiori2@gmail.com>
2025-06-16 21:15:52 -0300

    Updates

---
+++ aulas/06-13-2025-List-employee/src/entities/Employee.java
@@ -0,0 +1,50 @@
+package entities;
+
+public class Employee {
+    private Integer id;
+    private String name;
+    private Double salary;
+
+    public Employee(Integer id, String name, Double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double percent) {
+        salary += salary * percent / 100.0;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName() {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String toString(){
+        return id +
+                  ", "
+                + name
+                + ", "
+                + String.format("%.2f", salary);
+    }
+}
