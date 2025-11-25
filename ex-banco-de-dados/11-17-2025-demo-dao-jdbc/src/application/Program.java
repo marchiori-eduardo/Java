66727b7a6b5be8aa1e3479f3efe61da37486ed36
Eduardo Marchiori <marchiorimarchiori2@gmail.com>
2025-11-25 12:29:14 -0300

    findByDepartment Implementation

--- ex-banco-de-dados/11-17-2025-demo-dao-jdbc/src/application/Program.java
+++ ex-banco-de-dados/11-17-2025-demo-dao-jdbc/src/application/Program.java
@@ -7,6 +7,7 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
@@ -17,6 +18,13 @@ public class Program {
 
         System.out.println(seller);
 
+        System.out.println("\n=== TEST 2: seller findByDepartment ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+
+        for (Seller s : list) {
+            System.out.println(s);
+        }
 
     }
 }
